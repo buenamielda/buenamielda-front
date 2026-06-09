bf817a5437d0f3b3d890e014370ce4fb2f13cb1d
paulixx <paula.martinez.arce@alumnos.upm.es>
2026-06-09 09:43:45 +0200

    añadir consulta del detalle de un medido desde la gestión del admin buenamielda/buenamielda#83

--- src/app/models/admin-order.model.ts
+++ src/app/models/admin-order.model.ts
@@ -5,3 +5,29 @@ export interface AdminPedidoResponseDto {
   total: number;
   emailUsuario: string | null;
 }
+
+export interface AdminLineaPedidoResponseDto {
+  id: number;
+  cantidad: number;
+  nombreProducto: string | null;
+  nombreCategoria: string | null;
+  imagenProducto: string | null;
+  precioUnitario: number;
+  subtotal: number;
+}
+
+export interface AdminPedidoDetallesResponseDto {
+  id: number;
+  fechaPedido: string;
+  estado: string;
+  total: number;
+  emailUsuario: string | null;
+  nombreUsuario: string | null;
+  telefono: string | null;
+  direccion: string | null;
+  codigoPostal: string | null;
+  localidad: string | null;
+  provincia: string | null;
+  pais: string | null;
+  lineaPedido: AdminLineaPedidoResponseDto[];
+}
